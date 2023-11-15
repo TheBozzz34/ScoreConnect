aa88143a7ed48a0efd49a7bb440f184ff2bee3c9
Ethan James <necrozma@catgirlsaresexy.org>
2023-11-14 18:57:36 -0700

    Remove branding from head

--- pages/login.tsx
+++ pages/login.tsx
@@ -35,14 +35,7 @@ export default function Login() {
   return (
     <>
       <Head>
-        <meta property="og:url" content="https://next-enterprise.vercel.app/" />
-        <meta
-          property="og:image"
-          content="https://raw.githubusercontent.com/Blazity/next-enterprise/main/project-logo.png"
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:url" content="https://sc.necrozma.xyz" />
         <title>ScoreConnect Web</title>
       </Head>
       <NavBar />
--- pages/scoreboard/index.tsx
+++ pages/scoreboard/index.tsx
@@ -114,14 +114,7 @@ export default function Scoreboard() {
   return (
     <>
       <Head>
-        <meta property="og:url" content="https://next-enterprise.vercel.app/" />
-        <meta
-          property="og:image"
-          content="https://raw.githubusercontent.com/Blazity/next-enterprise/main/project-logo.png"
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:url" content="https://sc.necrozma.xyz" />
         <title>ScoreConnect Web</title>
       </Head>
 
