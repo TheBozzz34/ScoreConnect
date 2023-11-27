11132386b4ee9e44392cd2b2d6c116c44753357e
Ethan James <necrozma@catgirlsaresexy.org>
2023-11-26 21:49:11 -0700

    Update favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/tailwind.css"
 import { AppProps } from "next/app"
+import Head from "next/head"
 import Footer from "components/Footer/Footer"
 import { WebSocketProvider } from "../context/WebSocketContext"
 import { IsSsrMobileContext } from "../utils/useIsMobile"
@@ -8,6 +9,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <IsSsrMobileContext.Provider value={pageProps.isSsrMobile}>
       <WebSocketProvider>
+        <Head>
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        </Head>
         <Component {...pageProps} />
       </WebSocketProvider>
       <Footer />
