b4756bbafdcd06ea9d18cc1f2d0ebe70bb66cbcc
Ethan James <necrozma@catgirlsaresexy.org>
2023-11-26 20:32:41 -0700

    Fixed mobile footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -9,17 +9,17 @@ const Footer = () => {
     
     if (isMobile) {
         return (
-            <footer className="bg-[#454138] rounded-lg shadow m-4 text-[#dcd8c0] absolute bottom-0 left-0">
+            <footer className="bg-[#454138] rounded-lg shadow m-4 text-[#dcd8c0] sticky bottom-1 z-[100]">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                     <ul className="flex flex-wrap items-center text-sm font-medium sm:mt-0">
                         <li>
                             <a className="underline" href="https://metakirby5.github.io/yorha/">Theme</a> by <a href="https://github.com/metakirby5" className="underline me-4 md:me-6">metakirby5</a>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                            <a href="/about" className="hover:underline me-4 md:me-6">About</a>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline">Contact</a>
+                            <a href="/404" className="hover:underline">Contact</a>
                         </li>
                     </ul>
                 </div>
@@ -28,7 +28,7 @@ const Footer = () => {
     } else {
         return (
             <div >
-            <footer className="bg-[#454138] rounded-lg shadow m-4 text-[#dcd8c0] absolute bottom-0 right-0">
+            <footer className="bg-[#454138] rounded-lg shadow m-4 text-[#dcd8c0] fixed bottom-0 right-0 z-[100] w-fit">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0">
                         <li>
@@ -47,7 +47,7 @@ const Footer = () => {
                 </div>
             </footer>
 
-            <footer className="bg-[#454138] rounded-lg shadow m-4 text-[#dcd8c0] absolute bottom-0 left-0">
+            <footer className="bg-[#454138] rounded-lg shadow m-4 text-[#dcd8c0] fixed bottom-0 w-fit">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0">
                         <li>
