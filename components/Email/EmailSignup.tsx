b641e2dca482b147bf29d76bb8648460b1672581
Ethan James <necrozma@catgirlsaresexy.org>
2023-11-26 21:38:38 -0700

    Added last name field

--- components/Email/EmailSignup.tsx
+++ components/Email/EmailSignup.tsx
@@ -15,13 +15,22 @@ const EmailSignup: React.FC = () => {
           placeholder="you@example.com"
           className="mb-2 w-64 rounded-md bg-[#dcd8c0] p-2 text-[#454138] placeholder-[#454138] focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#bab5a1]"
         />
+        <div className="w-full flex flex-row justify-center">
         <input
           type="text"
           name="firstName"
-          placeholder="Joe"
-          className="mb-2 w-64 rounded-md bg-[#dcd8c0] p-2 text-[#454138] placeholder-[#454138] focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#bab5a1]"
+          placeholder="Jon"
+          className="mb-2 w-1/6 mr-4 rounded-md bg-[#dcd8c0] p-2 text-[#454138] placeholder-[#454138] focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#bab5a1]"
           required
         />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Snow"
+          className="mb-2 w-1/6 rounded-md bg-[#dcd8c0] p-2 text-[#454138] placeholder-[#454138] focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#bab5a1]"
+          required
+        />
+        </div>
         <input type="hidden" name="userGroup" value="Website signups" />
         <button
           type="submit"
